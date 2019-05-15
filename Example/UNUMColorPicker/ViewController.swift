94f6d90065719a71b3717d67ea4a4d9640010364
An Nguyen <an@unum.la>
2019-05-14 17:19:03 -0700

    Add background color in example to see Color Picker clearly, pin to edges of view

--- Example/UNUMColorPicker/ViewController.swift
+++ Example/UNUMColorPicker/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .lightGray
+
         //color data
         let colorData: [UIColor] = [.white, .black, .blue, .green, .yellow, .red, .cyan, .brown]
 
